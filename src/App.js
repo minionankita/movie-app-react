abfa3bafe0de38a82e8f36f28345c1214a18e7aa
Santhosh Kurian <a-8068@LE1421.local>
2020-09-02 18:22:12 +0530

    First commit- movie app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import Home from "./layout/Home.js";
+import Admin from "./layout/Admin.js";
+import User from "./layout/User.js";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact render={props => <Home {...props} />} />
+        <Route path="/admin" exact render={props => <Admin {...props} />} />
+        <Route path="/user" exact render={props => <User {...props} />} />
+      </Switch>
+    </BrowserRouter>
   );
 }
-
 export default App;
